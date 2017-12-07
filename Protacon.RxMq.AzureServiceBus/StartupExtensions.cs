35e22312a83f09fb3c81b40c67aefb98357ec80c
Savpek <savolainen.pekka@gmail.com>
2017-12-07 20:01:01 +0200

    Added extension method for startup.

---
+++ Protacon.RxMq.AzureServiceBus/StartupExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Protacon.RxMq.Abstractions;
+
+namespace Protacon.RxMq.AzureServiceBus
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddAzureBusMq(this IServiceCollection services, Action<MqSettings> configure)
+        {
+            services.Configure(configure);
+            services.AddTransient<AzureQueueManagement>();
+            services.AddSingleton<IMqSubscriber, AzureBusSubscriber>();
+            services.AddSingleton<IMqPublisher, AzureBusPublisher>();
+            return services;
+        }
+    }
+}
