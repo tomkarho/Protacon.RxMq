57997572b0e7ce68acaaeba414933df5d4fbcc50
Heikki-Jussi Niemi <hekku2@gmail.com>
2020-08-28 13:28:23 +0300

    Temporary commit for SP creation (not working as there are some parameter isseus)

--- Create-ClientSecrets.ps1
+++ Create-ClientSecrets.ps1
@@ -1,5 +1,6 @@
 param(
-    [Parameter()][string]$SettingsFile = 'developer-settings.json'
+    [Parameter()][string]$SettingsFile = 'developer-settings.json',
+    [Parameter()][Switch]$CreateServicePrincipal
 )
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
@@ -7,11 +8,38 @@ Set-StrictMode -Version Latest
 Write-Host "Reading settings from file $SettingsFile"
 $settingsJson = Get-Content -Raw -Path $SettingsFile | ConvertFrom-Json
 
+$context = Get-AzContext
+
+if ($CreateServicePrincipal -eq $true) {
+    $credentials = New-Object `
+        -TypeName Microsoft.Azure.Commands.ActiveDirectory.PSADPasswordCredential `
+        -Property @{ `
+            StartDate = Get-Date; `
+            EndDate   = '2099-01-01'; `
+            Password  = 'ExamplePassword!1'; `
+    
+    }`
+
+    New-AzAdServicePrincipal `
+        -DisplayName 'TestingServicePrincipal' `
+        -PasswordCredential $credentials `
+        -Role Reader `
+        -Scope "/subscriptions/$($context.Subscription.Id)/resourceGroups/$($settingsJson.ResourceGroupName)"
+}
+
 $busKey = Get-AzServiceBusKey -ResourceGroupName $settingsJson.ResourceGroupName -Namespace $settingsJson.ResourceGroupName -Name 'RootManageSharedAccessKey'
 
 $output = @{
-    ConnectionString   = $busKey.PrimaryConnectionString
-    AzureResourceGroup = $settingsJson.ResourceGroupName
+    ConnectionString         = $busKey.PrimaryConnectionString
+    AzureResourceGroup       = $settingsJson.ResourceGroupName
+    AzureSubscriptionId      = $context.Subscription.Id
+    AzureSpAppId             = $settingsJson.ServicePrincipal.AppId
+    AzureSpPassword          = $settingsJson.ServicePrincipal.Password
+    AzureSpTenantId          = $settingsJson.ServicePrincipal.TenantId
+    AzureNamespace           = $settingsJson.ResourceGroupName
+    AzureRetryMinimumBackoff = 5
+    AzureRetryMaximumBackoff = 10
+    AzureMaximumRetryCount   = 10
 }
 
 $output | ConvertTo-Json -depth 100 | Out-File "Protacon.RxMq.AzureServiceBus.Tests\client-secrets.json"
