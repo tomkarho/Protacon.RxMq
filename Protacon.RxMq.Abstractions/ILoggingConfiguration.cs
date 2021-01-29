70ec5cf3742f98a448ff9c1cc452af4c0c72eae9
jussivesa <jussi.vesa@gmail.com>
2021-01-29 08:10:09 +0200

    Implement support for logging configration
    - Support configuration where implementation is done in the project referencing this configuration
    - Exclude by queue name from logging
    - Exclude by topic name from logging

---
+++ Protacon.RxMq.Abstractions/ILoggingConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Protacon.RxMq.Abstractions
+{
+    public interface ILoggingConfiguration
+    {
+        /// <summary>
+        /// Get configured queue names to be excluded from logging.
+        /// </summary>
+        /// <returns>List of queue names</returns>
+        IList<string> ExcludeQueuesFromLogging();
+        /// <summary>
+        /// Get configured topic names to be excluded from logging.
+        /// </summary>
+        /// <returns>List of topic names</returns>
+        IList<string> ExcludeTopicsFromLogging();
+    }
+}
---
+++ Protacon.RxMq.AzureServiceBusLegacy.Tests/LoggingConfigurationUnitTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Protacon.RxMq.AzureServiceBusLegacy.Tests
+{
+    public class LoggingConfigurationUnitTests : IDisposable
+    {
+        private const string ConfigFileName = "logging-configuration.json";
+
+        public void Dispose()
+        {
+            try
+            {
+                var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+                FileInfo fileInfo = new FileInfo(fileName);
+                fileInfo.Delete();
+            }
+            catch
+            {
+            }
+        }
+
+        [Fact]
+        public void WhenInitializedWithoutConfiguration_ThenLoggingFilterIsEmpty()
+        {
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(0);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInitializedWithEmptyConfiguration_ThenLoggingFilterIsEmpty()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (File.Create(Path.Combine(fileName)))
+            {
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(0);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInitializedWithInvalidConfiguration_ThenLoggingFilterIsEmpty()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (FileStream fs = File.Create(Path.Combine(fileName)))
+            {
+                var content = new UTF8Encoding(true).GetBytes("{\"NoQueuesKeyHere\": \"invalid\", \"NoTopicsKeyHere\": \"invalid\"}");
+                fs.Write(content, 0, content.Length);
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(0);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInitializedWithCorrentKeyConfiguration_WithInvalidData_ThenLoggingFilterIsEmpty()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (FileStream fs = File.Create(Path.Combine(fileName)))
+            {
+                var content = new UTF8Encoding(true).GetBytes("{\"Queues\": \"invalid\", \"topics\": \"invalid\"}");
+                fs.Write(content, 0, content.Length);
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(0);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInitializedWithCorrentKeyConfiguration_WithEmptyData_ThenLoggingFilterIsEmpty()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (FileStream fs = File.Create(Path.Combine(fileName)))
+            {
+                var content = new UTF8Encoding(true).GetBytes("{\"Queues\": [], \"Topics\": []}");
+                fs.Write(content, 0, content.Length);
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(0);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInitializedWithCorrentKeyConfiguration_WithEmptyValues_ThenLoggingFilterIsEmpty()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (FileStream fs = File.Create(Path.Combine(fileName)))
+            {
+                var content = new UTF8Encoding(true).GetBytes("{\"Queues\": [\"\",\"\"], \"Topics\": [\"\",\"\"]}");
+                fs.Write(content, 0, content.Length);
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(0);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEmpty();
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(0);
+        }
+
+        [Fact]
+        public void WhenInitializedWithCorrentKeyConfiguration_WithValues_ThenLoggingFilterIsSet()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (FileStream fs = File.Create(Path.Combine(fileName)))
+            {
+                var content = new UTF8Encoding(true).GetBytes("{\"Queues\": [\"testqueue\",\"anothertestqueue\"], \"Topics\": [\"testtopic\",\"anothertesttopic\"]}");
+                fs.Write(content, 0, content.Length);
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEquivalentTo("testqueue", "anothertestqueue");
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(2);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEquivalentTo("testtopic", "anothertesttopic");
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(2);
+        }
+
+        [Fact]
+        public void WhenInitializedWithCorrentKeyConfiguration_WithMixedValues_ThenLoggingFilterIsSet()
+        {
+            var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ConfigFileName);
+            using (FileStream fs = File.Create(Path.Combine(fileName)))
+            {
+                var content = new UTF8Encoding(true).GetBytes("{\"Invalid\": \"Values\",\"Queues\": [\"\", \"testqueue\", \"       \", \"anothertestqueue\"], \"Topics\": [\"\", \"testtopic\", \"       \", \"anothertesttopic\"]}");
+                fs.Write(content, 0, content.Length);
+            }
+
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.ExcludeQueuesFromLogging().Should().BeEquivalentTo("testqueue", "anothertestqueue");
+            loggingConfiguration.ExcludeQueuesFromLogging().Count.Should().Be(2);
+            loggingConfiguration.ExcludeTopicsFromLogging().Should().BeEquivalentTo("testtopic", "anothertesttopic");
+            loggingConfiguration.ExcludeTopicsFromLogging().Count.Should().Be(2);
+        }
+    }
+}
