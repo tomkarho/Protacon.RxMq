86cb2de28919d29e73ab92fc16e064db869616ae
jussivesa <jussi.vesa@pinja.com>
2021-11-18 08:09:28 +0200

    Fix script parameter "EnvironmentName" and read it from file instead - Script did not work as settings were not read to object before accessing it in script parameters block - Add some logging

--- Create-ClientSecrets.ps1
+++ Create-ClientSecrets.ps1
@@ -1,29 +1,33 @@
 param(
-    [Parameter()][string]$SettingsFile = 'developer-settings.json',
-    [Parameter()][string]$EnvironmentName = $SettingsFile.ResourceGroupName
+    [Parameter()][string]$SettingsFile = 'developer-settings.json'
 )
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
-Write-Host "Reading settings from file $SettingsFile"
+Write-Host "Reading settings from file '$SettingsFile'"
 $settingsJson = Get-Content -Raw -Path $SettingsFile | ConvertFrom-Json
 
 $context = Get-AzContext
 
-$busKey = Get-AzServiceBusKey -ResourceGroupName $EnvironmentName -Namespace $EnvironmentName -Name 'RootManageSharedAccessKey'
+$busKey = Get-AzServiceBusKey -ResourceGroupName $settingsJson.ResourceGroupName -Namespace $settingsJson.ResourceGroupName -Name 'RootManageSharedAccessKey'
 
 $output = @{
     ConnectionString         = $busKey.PrimaryConnectionString
-    AzureResourceGroup       = $EnvironmentName
+    AzureResourceGroup       = $settingsJson.ResourceGroupName
     AzureSubscriptionId      = $context.Subscription.Id
     AzureSpAppId             = $settingsJson.ServicePrincipal.AppId
     AzureSpPassword          = $settingsJson.ServicePrincipal.Password
     AzureSpTenantId          = $context.Tenant.Id
-    AzureNamespace           = $EnvironmentName
+    AzureNamespace           = $settingsJson.ResourceGroupName
     AzureRetryMinimumBackoff = 5
     AzureRetryMaximumBackoff = 10
     AzureMaximumRetryCount   = 10
 }
 
-$output | ConvertTo-Json -depth 100 | Out-File "Protacon.RxMq.AzureServiceBus.Tests\client-secrets.json"
-Copy-Item "Protacon.RxMq.AzureServiceBus.Tests\client-secrets.json" -Destination "Protacon.RxMq.AzureServiceBusLegacy.Tests"
+$filename = "client-secrets.json"
+$projectLocation = "Protacon.RxMq.AzureServiceBus.Tests"
+$legacyProjectLocation = "Protacon.RxMq.AzureServiceBusLegacy.Tests"
+$output | ConvertTo-Json -depth 100 | Out-File "$projectLocation\$filename"
+Copy-Item "$projectLocation\$filename" -Destination $legacyProjectLocation
+
+Write-Host "Write file '$filename' to '$projectLocation', '$legacyProjectLocation'"
